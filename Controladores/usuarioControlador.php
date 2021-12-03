baef3dbd5a1dbdaab49d287f2d55bcc9c7ebb19e
Miguel Méndez <douglas.figueroa20@itca.edu.sv>
2021-12-02 23:00:39 -0600

    completando login con recuerdame y no recuerdame

--- Controladores/usuarioControlador.php
+++ Controladores/usuarioControlador.php
@@ -17,66 +17,97 @@ if($_POST){
                 $nombre = $data["txtUsuario"];
                 $contraseña = $data["txtContraseña"];
                 $rem = $_POST["valor"];
+
                 //vemos si ya hay cookies de recuerdame
                 if(isset($_COOKIE["usuario"]) && isset($_COOKIE["token"])){
-                    $usuarioValidoCok= $usDao->validarUsuarioCookie($nombre,$contraseña);
-                    if($usuarioValidoCok){
-                        $numero_aleatorio = mt_rand(1000000,999999999);
-                        $token = ($numero_aleatorio +1);
-
-                        $actualizarToken= $usDao->setUserToken($nombre,$token);
-                        if($actualizarToken){
-                            setcookie("token", $token, time()+30*24*60*60);
-                            echo json_encode(true);
+                    //si hay cookies de recuerdame veo si el checkbox esta seleccionado
+                    if($rem==0){
+                        //si no esta seleccionado ya no quiere ser recordado
+                        //destruyo las cookies
+                        setcookie("usuario", "", time()-60);
+                        setcookie("token", "", time()-60);
+                        //actualizo a null el valor del token del usuario en BD
+                        $updateToken= $usDao->setUserToken($nombre,null);
+                        if($updateToken){
+                            //si se actualizo correctamente el campo
+                            //comprobamos datos de las cookies
+                            $userValid= $usDao->validarUsuarioCookie($nombre,$contraseña);
+                            if($userValid){
+                                //si esta todo bien con lso datos de la cookie retornamos true
+                                echo json_encode(true);
+                            }else{
+                                // si fallo con los datos de la cookie
+                                echo json_encode(false);
+                            }
                         }else{
+                            //si no se actualizo el campo
                             echo json_encode(false);
                         }
-                        
                     }else{
+                        //si el checkbox de recuerdame esta seleccionado compruebo datos
+                        $usuarioValidoCok= $usDao->validarUsuarioCookie($nombre,$contraseña);
+                        if($usuarioValidoCok){
+                            //si los datos estan bien, genero un nuevo token
+                            $numero_aleatorio = mt_rand(1000000,999999999);
+                            $token = ($numero_aleatorio +1);
+                            //actualizo el campo del token del usuario
+                            $actualizarToken= $usDao->setUserToken($nombre,$token);
+                            if($actualizarToken){
+                                //si se actualizo el campo, paso el valor a la cookie token
+                                setcookie("token", $token, time()+30*24*60*60);
+                                echo json_encode(true);
+                            }else{
+                                //si no se actualiza el token
+                                echo json_encode(false);
+                            }
+                            
+                        }else{
+                            //si los datos de la cookie no son validos
+                            echo json_encode(false);
+                        }
+                    }
+                 }else{
+                     //si no hay cookies
+                    //validamos si hay usuario con los datos ingresados
+                    $usuarioExiste= $usDao->validarUsuario($nombre,$contraseña);
+
+                    if($usuarioExiste){
+                        //si hay datos vemos si quiere ser recordado
+                        if($rem==1){
+                            
+                            //si es que queire ser recordado
+                            //creamos un número aleatorio para usarlo como "token"
+                            $numero_aleatorio = mt_rand(1000000,999999999);
+                            $token = ($numero_aleatorio +1);
+
+                            //actualizo el token del usuario en la BD
+                            $saveToken= $usDao->setUserToken($nombre, $token);
+                            
+                            if($saveToken){
+
+                                //si se actualizo creo las cookie por un mes
+                            
+                                setcookie("usuario", $_SESSION["usuario"]["nombre"], time()+30*24*60*60 );
+                                setcookie("token", $token, time()+30*24*60*60);
+                                
+                                //retornamos true para redireccionar con ajax en succes
+                                echo json_encode(true);
+                            }else{
+                                //retornamos false para que muestre error con ajax
+                                echo json_encode(false);
+                            }
+                            
+                        }else{
+                            //si no quiere ser recordado simplemente
+                            //retornamos true para que redireccione ajax
+                            echo json_encode(true);
+                        }
+                    }else{
+                        //si no hay usuario con los datos ingresados
+                        //retornamos falso
                         echo json_encode(false);
                     }
-                    die;
-                   
                 }
-                //validamos si hay usuario con los datos ingresados
-                $usuarioExiste= $usDao->validarUsuario($nombre,$contraseña);
-
-                if($usuarioExiste){
-                    //si hay datos vemos si quiere ser recordado
-                    if($rem==1){
-                        
-                        //si es que queire ser recordado
-                        //creamos un número aleatorio para usarlo como "token"
-                        $numero_aleatorio = mt_rand(1000000,999999999);
-                        $token = ($numero_aleatorio +1);
-
-                        //actualizo el token del usuario en la BD
-                        $saveToken= $usDao->setUserToken($nombre, $token);
-                        
-                        if($saveToken){
-
-                            //si se actualizo creo las cookie por un mes
-                         
-                            setcookie("usuario", $_SESSION["usuario"]["nombre"], time()+30*24*60*60 );
-                            setcookie("token", $token, time()+30*24*60*60);
-                            
-                            //retornamos true para redireccionar con ajax en succes
-                            echo json_encode(true);
-                        }else{
-                            //retornamos false para que muestre error con ajax
-                            echo json_encode(false);
-                        }
-                        
-                    }else{
-                        //si no esta marcado recuerdame, evaluamos si hay cookies 
-                        echo json_encode("olvidame");
-                    }
-                }else{
-                    //si no hay usuario con los datos ingresados
-                    //retornamos falso
-                    echo json_encode(false);
-                }
-                
             break;
             case "cerrarSesion":
                 session_destroy();
--- Recursos/JS/loginjs.js
+++ Recursos/JS/loginjs.js
@@ -1,9 +1,16 @@
 $(document).ready(function(){
+    var rem= 0;
     $("#codeContent").hide();
     $("#labelError").hide();
-    
-    comprobarCokieRememberme();
 
+    comprobarCokieRememberme();
+    $('#customCheck').change(function(){
+        if(!$('#customCheck').is(":checked")){
+            $('#customCheck').attr("checked",false);
+            rem=0;
+        }
+
+    });
     //cuando hace click en btnLogin 
     $("#btnLogin").on("click", function(e){
         //se evita el evento envio del form
@@ -13,7 +20,7 @@ $(document).ready(function(){
         var data = $("#frmLogin").serialize();
         
         //obtenemos el valor del ckeckbox recordarme
-        var rem= 0;
+        
         if ($('#customCheck').is(":checked"))
         {
             rem= 1;
@@ -29,7 +36,7 @@ $(document).ready(function(){
                 console.log(r);
                 //en caso de comunicación exitosa, comprobamos valor booleano de respuesta
                 //que viene del servidor
-                if(r){
+         /*      if(r){
                     //si todo esta correcto, redireccionamos al inicio
                     $(location).attr('href',"vistas/home.php");
                 }else{
@@ -47,7 +54,7 @@ $(document).ready(function(){
                     $('input[name="txtUsuario"]').keypress(function(){
                         $("#labelError").hide();
                     });
-                } 
+                }*/
             },
             error: function (r) {
                 //si falla algo se muestra error de conexión en el servidor
