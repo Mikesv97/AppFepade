96a97ad9c49f0ff26cc46d019ebaa03245c8c457
Karsa <castillo.alan1995@gmail.com>
2021-12-04 23:50:45 -0600

    Ya inserta activo fijo

--- Recursos/JS/plugins-init/jquery-steps-init.js
+++ Recursos/JS/plugins-init/jquery-steps-init.js
@@ -12,6 +12,25 @@
         {
             form.validate().settings.ignore = ":disabled,:hidden";
             return form.valid();
+        },
+        onFinished: function(event, currentIndex)
+        {
+            var data = $(form).serialize();
+            $.ajax({
+                url: '../Controladores/activoFijoControlador.php',
+                method: "post",
+                dataType: "json",
+                data: { "key": "insertar", "data": data },
+                success: function (r) {
+                    console.log(r);
+                    if(r){
+                        alert ('INSERTADO');
+                    }
+                },
+                error: function (r) {
+                    console.log(r);
+                }
+            });
         }
     });
 
--- Recursos/JS/scriptPlantilla/activoFijo.js
+++ Recursos/JS/scriptPlantilla/activoFijo.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
 
-    $('a[role="menuitem"]').on('click',function(){
-        var alerta = $('li').attr("href");
-        if(alerta = '#finish'){
-            alert(alerta);
-        }
+    // $('a[role="menuitem"]').on('click',function(){
+    //     var alerta = $('li').attr("href");
+    //     if(alerta = '#finish'){
+    //         alert(alerta);
+    //     }
         
-    });
+    // });
 
 });
