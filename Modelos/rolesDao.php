c8b1f80422cd2fb85ef62d720ea3fa47696ae94e
Karsa <castillo.alan1995@gmail.com>
2021-12-10 19:21:43 -0600

    Ya imprime datatable con ajax

--- Modelos/rolesDao.php
+++ Modelos/rolesDao.php
@@ -11,7 +11,7 @@ class RolesDao{
     }
 
     public function conectar(){
-        $serverName = "DESKTOP-VAIT65I\SQLEXPRESS";
+        $serverName = "DESKTOP-CO34HBA\SQLEXPRESS";
         $basedatos="ACTIVO";
         try{
            
