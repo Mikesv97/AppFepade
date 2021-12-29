306d5e98fc390134108b0805d11e38fca859ab64
Miguel Méndez <douglas.figueroa20@itca.edu.sv>
2021-12-29 17:00:00 -0600

    ya carga las acciones seleccionadas

--- Recursos/JS/rolesPermisos.js
+++ Recursos/JS/rolesPermisos.js
@@ -4,11 +4,8 @@ $(document).ready(function ($) {
     cargarAcciones();
     cargarMenus();
 
-    /*Cuando se genere clic en el último elemento de la lista dentro del ID grupoCkbAcciones
-    desmarcamos los demás checkbox seleccionados, ya que el último elemento de la lista pertenece
-    a "Ninguna" lo que significa que el rol no podrá hacer ninguna acción en el sistema, porque así 
-    está en la tabla acciones en la BD, si esto se cambia, y la acción "ninguna" deja de ser
-    el último elemento, se debe ajustar al elemento que sea, ejm: first, second, etc. */
+    //cuando se genere click en el último elemento de la lista
+    //desmarcamos los demás chekbox de la misma lista (más detalles dentro de la función, al final.)
     $("body").on("click","#grupoCkbAcciones li:last", function(){
 
         /*como se hace clic en el último elemento de la lista que pertenece a ninguno obtenemos su valor */
@@ -28,28 +25,42 @@ $(document).ready(function ($) {
                 }
             }
         });
-        } 
+        }
+
+        /* -----DETALLE DE LA LÓGICA DE LA FUNCIÓN */
+        /*Cuando se genere clic en el último elemento de la lista de ID grupoCkbAcciones
+        desmarcamos los demás checkbox seleccionados, ya que el último elemento de la lista pertenece
+        a "Ninguna" lo que significa que el rol no podrá hacer ninguna acción en el sistema, porque así 
+        lo definimos en la tabla acciones en la BD, si esto se cambia, y la acción "ninguna" deja de ser
+        el último elemento, se debe ajustar al elemento que sea, ejm: first, second, etc. O en caso no la
+        agreguen es de comentar/eliminar esta función */
+    });
+
+    /*lo contrario a lo del evento click del último elemento de la lista de ID grupoCkbAcciones*/
+    $("body").on("click","#grupoCkbAcciones li", function(){
+        //obtenemos el número total de elementos inputs tipo chekbox con clase .ckbAcciones
+        var totalCkbAcc =  $(".ckbAcciones:checkbox").length;
+        //preguntamos si el index donde se hizo click es el mismo del total (que viene siendo el último)
+        if($(this).index() != totalCkbAcc){
+            //en caso sea diferente, quiere decir que hizo click en otro chekbox
+            //diferente a "Ninguna", procedemos a desmarcar el chekbox "Ninguna"
+            $(".ckbAcciones:checkbox:last").prop('checked',false);
+        }
 
     });
 
     
-    //al submit evaluamos si los chekbox están seleccionados almenos 1 de cada tipo
+    //cuando se ejecuta el submit del form roles
     $("#frmRoles").submit(function(e){
+        //evitamos el evento
         e.preventDefault();
-        var ckbAcc =$(".ckbAcciones:checkbox");
-        var countCkbAcc = parseInt(0);
-        var totalCkbAcc =  ckbAcc.length;
-        //verificamos si almenos un ckbx de acción está seleccionado
-        $(".ckbAcciones:checkbox").each(function(index, elemento){
-            if($(elemento).is(":checked")){
-                if(index != (totalCkbAcc-1)){
-                    console.log("se suma la accion "+$(elemento).next("label").text()+" con el valor: "+$(elemento).val());
-                }else{
-                    console.log("Solo se suma "+$(elemento).next("label").text()+" con el valor: "+$(elemento).val());
-                }
-            
-            }
-        });
+        
+        //obtenemos el array con las acciones seleccionadas para el rol
+       var accionesArray = generarArrayAcciones();
+
+        for(let i=0; i<accionesArray.length; i++){
+            console.log("Seleccionaste el item #: "+i+" y su valor es: "+accionesArray[i]);
+        }
     });
 
     //función que solicita por ajax la carga de roles
@@ -163,5 +174,36 @@ $(document).ready(function ($) {
                 })
             }
         });          
+    }
+
+    /* esta función recorre todos los chekbox con clase .ckbAcciones
+       crea un array con cada valor seleccionado y lo retorna  */
+    function generarArrayAcciones(){
+        //obtenemos el total de elementos inputs tipo chekbox con clase ckbAcciones
+        var totalCkbAcc = $(".ckbAcciones:checkbox").length;
+        //creamos el array
+        var accionesArray = [];
+        //creamos contador para ir manejando el número de elementos seleccionados
+        var count = parseInt(0);
+        //recorremos y verificamos si almenos un ckbx de acción está seleccionado
+        $(".ckbAcciones:checkbox").each(function(index, elemento){
+            if($(elemento).is(":checked")){
+                //si está seleccionado el elemento actual del recorrido
+                //evaluamos si es el último o no
+                if(index != (totalCkbAcc-1)){
+                    // si no es el último, asignamos la posicion de count con el valor del elemento actual
+                    accionesArray[count] = $(elemento).val();
+                    //incrementamos en 1 count
+                    count++;
+                }else{
+                    //en caso sea el último simplemente se crea el array
+                    //con posición count (será 0 ya que si el último está seleccionado, los demás se desmarcan)
+                    accionesArray[count] = $(elemento).val();
+                }
+            
+            }
+        });
+        //retornamos el arreglo
+        return accionesArray;
     }
 });
